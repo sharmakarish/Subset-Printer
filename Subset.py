4bf6e183a42ec638da55b0c78ae067ddeaa438c4
Pallav <69533542+PallavRastogi@users.noreply.github.com>
2021-05-04 19:30:55 +0530

    Create Subset.py
    Over Reduced code to print all subsets of a given list.
    only 5 lines required!

---
+++ Subset.py
@@ -0,0 +1,5 @@
+def subset(items):
+    for i in range(2**len(items)):
+        for t, po in enumerate(list(((len(items)-len(str(bin(i))[2:]))*"0") + str(bin(i))[2:])):
+            if po == "1": print(items[t], end = " ")                
+        print("\n")
